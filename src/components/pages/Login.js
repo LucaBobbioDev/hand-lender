667370f4c6f5b146db5d8839d5ba0bd1a638a959
Luca Bobbio <bobbio.luca.117@gmail.com>
2022-05-28 20:44:44 -0300

    Modificações no Login
    Alterei os arquivos Login.js e o Login.module.css

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,21 +1,30 @@
 import Card from "../layout/Card"
 import styles from './Login.module.css';
 import login from '../../img/login.svg'
+import Section from '../layout/Section'
 
 export default function Login(){
     return(
         <>
-            <div className={styles.area}>
-                <Card>
-                    Bem vindo!
-                    <div>
-                        <input type="email" placeholder="Insira o seu email"/>
-                        <input type="password" placeholder="Insira a sua senha"/>
-                        <input type="submit"/>
-                    </div>
-                </Card>
-                <img src={login} width={300}></img>
-            </div>
+            <Section>
+                <div className={styles.area}>
+                    <Card>
+                        <div className={styles.conteudo}>
+                            <h3>Bem Vindo!</h3>
+                            <div className={styles.input}>
+                                <input type="email" placeholder="Insira o seu email"/>
+                            </div>
+                            <div className={styles.input}>
+                                <input type="password" placeholder="Insira a sua senha"/>
+                            </div>
+                            <div className={styles.input}>
+                                <input type="submit"/>
+                            </div>
+                        </div>
+                    </Card>
+                    <img src={login} width={300} alt='Drawing of a man opening a door'></img>
+                </div>
+            </Section>
         </>
     )
 }
