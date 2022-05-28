e43412e731746d91c30b7e39f2fcabcf112a2035
Luca Bobbio <bobbio.luca.117@gmail.com>
2022-05-28 20:36:09 -0300

    Novos componentes em pages

---
+++ src/components/pages/Create.js
@@ -0,0 +1,19 @@
+import create from '../../img/create.svg'
+import styles from './Create.module.css'
+import Section from '../layout/Section'
+
+export default function Create(){
+    return(
+        <>
+            <Section>
+                <h1>
+                    Criação do Projeto
+                </h1>
+                <p>
+                    Área de Criação do Projeto.
+                </p>
+                <img src={create} width={300} alt='Drawing of a man sculpting a bust'></img>
+            </Section>
+        </>
+    )
+}
