5514d019c0613d523eb45f6ae73ef59541dd935b
Luca Bobbio <bobbio.luca.117@gmail.com>
2022-05-28 20:42:30 -0300

    Modificações na Home
    Alterei o Home.js e o Home.module.css

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,22 +2,25 @@ import projects from '../../img/projects.svg'
 import styles from './Home.module.css'
 import {FaPlus} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
+
 export default function Home(){
     return(
         <>
         <div className={styles.section}>
-            <h1>
+                <h1>
                     Home
                 </h1>
                 <p>
                     Bem vindo ao Hand Lender!
                     Comece a publicar o seus projetos agora mesmo.
                 </p>
-                <img src={projects} width={300}></img>
-                <Link to="/">
-                    Criar Projeto
-                    <FaPlus/>
-                </Link>   
+                <img src={projects} width={300} alt='Drawing of a man using an laptop with a window behind'></img>
+                <div className={styles.criar}>
+                    <Link to="/Create">
+                        Criar Projeto
+                        <FaPlus/>
+                    </Link>
+                </div>   
         </div>
         </>
     )
